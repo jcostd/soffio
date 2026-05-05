c2adf1a7f84662f395fc741eb2d2d2e26982dbd7
jcostd <jacopocostantini32@gmail.com>
2026-05-05 13:22:44 +0200

    preview: added -dir and -port flags for better flexibility
    - Add README.org

--- cmd/preview/main.go
+++ cmd/preview/main.go
@@ -1,30 +1,27 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
-	"os"
-	"path/filepath"
 	"time"
 )
 
 func main() {
-	exe, err := os.Executable()
-	if err != nil {
-		log.Fatalf("fatal: resolve executable: %v", err)
-	}
-
-	publicDir := filepath.Join(filepath.Dir(exe), "public")
+	// Default to "public" but allow override
+	dir := flag.String("dir", "public", "directory to serve")
+	port := flag.String("port", "8080", "port to listen on")
+	flag.Parse()
 
 	go func() {
 		time.Sleep(500 * time.Millisecond)
-		openBrowser("http://localhost:8080")
+		openBrowser("http://localhost:" + *port)
 	}()
 
-	log.Printf("serve: %s at http://localhost:8080", publicDir)
+	log.Printf("serve: %s at http://localhost:%s", *dir, *port)
 	log.Printf("interrupt to exit")
 
-	if err := http.ListenAndServe(":8080", http.FileServer(http.Dir(publicDir))); err != nil {
+	if err := http.ListenAndServe(":"+*port, http.FileServer(http.Dir(*dir))); err != nil {
 		log.Fatalf("fatal: server: %v", err)
 	}
 }
